9d45603504368f2de9f6eafde5ecdf5b5cc59a4c
Paul de Rosanbo <paul.derosanbo@cantico.fr>
2015-09-20 21:45:05 +0200

    no nothing if not ready

--- src/angular-teleperiod.js
+++ src/angular-teleperiod.js
@@ -61,61 +61,67 @@
                         attrs.ready = 'true';
                     }
 
-                    teleperiodScope.d3Svg = d3.select(scope.svg[0]);
-                    scope.focusDate = attrs.focusDate || new Date();
-                    var selectedEvents = [];
+
+                    function initTeleperiod()
+                    {
+
+                        teleperiodScope.d3Svg = d3.select(scope.svg[0]);
+                        scope.focusDate = attrs.focusDate || new Date();
+                        var selectedEvents = [];
 
 
-                    if (undefined !== attrs.selectedevents) {
+                        if (undefined !== attrs.selectedevents) {
 
-                        var eventList = $parse(attrs.selectedevents)(scope);
-                        if (undefined !== eventList) {
-                            eventList.forEach(function(evt) {
-                                if (typeof evt === 'string') {
-                                    selectedEvents.push(evt);
-                                } else if (undefined !== evt.uid) {
-                                    selectedEvents.push(evt.uid);
-                                }
+                            var eventList = $parse(attrs.selectedevents)(scope);
+                            if (undefined !== eventList) {
+                                eventList.forEach(function(evt) {
+                                    if (typeof evt === 'string') {
+                                        selectedEvents.push(evt);
+                                    } else if (undefined !== evt.uid) {
+                                        selectedEvents.push(evt.uid);
+                                    }
 
-                            });
+                                });
+                            }
                         }
+
+                        function updateScope(selection) {
+                            $parse(attrs.dtstart).assign(scope, selection.dtstart);
+                            $parse(attrs.dtend).assign(scope, selection.dtend);
+
+                            if (undefined !== attrs.periods) {
+                                $parse(attrs.periods).assign(scope, selection.getValidPeriods());
+                            }
+                        }
+
+
+
+                        teleperiodScope.teleperiod = new Teleperiod({
+                            object: teleperiodScope.d3Svg,
+                            focusDate: scope.focusDate,
+                            selectedEvents: selectedEvents,
+                            workingtimes: function(interval) {
+                                return teleperiodScope.getPromisedData(attrs.workingtimes, interval);
+                            },
+
+                            events: function(interval) {
+                                return teleperiodScope.getPromisedData(attrs.events, interval);
+                            },
+
+                            onUpdated: function(selection) {
+                                scope.$apply(function() {
+                                    updateScope(selection);
+                                });
+                            }
+                        });
+
+
                     }
-
-                    function updateScope(selection) {
-                        $parse(attrs.dtstart).assign(scope, selection.dtstart);
-                        $parse(attrs.dtend).assign(scope, selection.dtend);
-
-                        if (undefined !== attrs.periods) {
-                            $parse(attrs.periods).assign(scope, selection.getValidPeriods());
-                        }
-                    }
-
-
-
-                    teleperiodScope.teleperiod = new Teleperiod({
-                        object: teleperiodScope.d3Svg,
-                        focusDate: scope.focusDate,
-                        selectedEvents: selectedEvents,
-                        workingtimes: function(interval) {
-                            return teleperiodScope.getPromisedData(attrs.workingtimes, interval);
-                        },
-
-                        events: function(interval) {
-                            return teleperiodScope.getPromisedData(attrs.events, interval);
-                        },
-
-                        onUpdated: function(selection) {
-                            scope.$apply(function() {
-                                updateScope(selection);
-                            });
-                        }
-                    });
-
-
 
 
                     scope.$watch(attrs.ready, function(newValue) {
                         if (true === newValue) {
+                            initTeleperiod();
                             teleperiodScope.teleperiod.draw();
                         }
                     }, true);
@@ -124,7 +130,7 @@
                     
                     scope.$watch(attrs.refreshevents, function(newValue) {
 
-                        if (newValue) {
+                        if (newValue && undefined !== teleperiodScope.teleperiod) {
                             teleperiodScope.teleperiod.refreshEvents();
                         }
                         
@@ -133,6 +139,10 @@
 
 
                     scope.$watch(attrs.dtstart, function(newValue) {
+
+                        if ( undefined === teleperiodScope.teleperiod) {
+                            return;
+                        }
 
                         var selection = teleperiodScope.teleperiod.selection;
 
@@ -144,6 +154,10 @@
                     }, true);
 
                     scope.$watch(attrs.dtend, function(newValue) {
+
+                        if ( undefined === teleperiodScope.teleperiod) {
+                            return;
+                        }
 
                         var selection = teleperiodScope.teleperiod.selection;
 
@@ -158,6 +172,10 @@
                     scope.$watch(attrs.selectedevents, function(newValue) {
 
                         if (undefined === newValue) {
+                            return;
+                        }
+
+                        if ( undefined === teleperiodScope.teleperiod) {
                             return;
                         }
 
